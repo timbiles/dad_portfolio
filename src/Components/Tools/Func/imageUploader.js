8ec2beb1a814e860e5b051dac5a283c98b4ed0b5
Tim Biles <timbilestimbiles@gmail.com>
2019-07-13 11:11:35 -0500

    upgrade dropzone package

--- src/Components/Tools/Func/imageUploader.js
+++ src/Components/Tools/Func/imageUploader.js
@@ -2,55 +2,46 @@ import React, { Component, Fragment } from 'react';
 import Dropzone from 'react-dropzone';
 
 class imageUploader extends Component {
-  
-
   onImageDrop = files => {
     this.props.imageUpload(files[0]);
   };
 
- 
   render() {
     return (
-       
-      <Dropzone
-        onDrop={this.onImageDrop}
-        className="ce_image_dropzone"
-      >
-      {({getRootProps, getInputProps, isDragActive}) => {
+      <Dropzone onDrop={this.onImageDrop} className="ce_image_dropzone">
+        {({ getRootProps, getInputProps, isDragActive }) => {
           return (
             <div
               {...getRootProps()}
-              className='image_uploader'
-            //   className={classNames('dropzone', {'dropzone--isActive': isDragActive})}
+              className="image_uploader"
+              //   className={classNames('dropzone', {'dropzone--isActive': isDragActive})}
             >
-            {this.props.image === '' ? (
+              {this.props.image === '' ? (
                 <Fragment>
-
-                    <input {...getInputProps()} />
-                    {
-                      isDragActive ?
-                        <p>Drop files here...</p> :
-                        <p>Try dropping some files here, or click to select files to upload.</p>
-                    }
+                  <input {...getInputProps()} />
+                  {isDragActive ? (
+                    <p>Drop files here...</p>
+                  ) : (
+                    <p>
+                      Try dropping some files here, or click to select files to
+                      upload.
+                    </p>
+                  )}
                 </Fragment>
-            ) : (
+              ) : (
                 <div className="image_uploader_container">
-                <img
-                  className="ep_upload_pic"
-                  src={this.props.image}
-                  alt="event pic"
-                />
-              </div>
-            )}
+                  <img
+                    className="ep_upload_pic"
+                    src={this.props.image}
+                    alt="event pic"
+                  />
+                </div>
+              )}
             </div>
-          )
+          );
         }}
 
-
-
-
-
-      {/* {({getRootProps, getInputProps, isDragActive}) => {
+        {/* {({getRootProps, getInputProps, isDragActive}) => {
           return <div 
         //   className='image_uploader'
           {...getRootProps()}
@@ -71,7 +62,6 @@ class imageUploader extends Component {
             )}
           </div>
       }} */}
-        
       </Dropzone>
     );
   }
